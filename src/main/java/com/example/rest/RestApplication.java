5bf71f54109025cb2e880bd9f83e472417b0bc4b
Safran S.M <IT24102097@my.sliit.lk>
2026-08-13 01:06:42 +0530

    Adding RestApplication.java

---
+++ src/main/java/com/example/rest/RestApplication.java
@@ -0,0 +1,17 @@
+package com.example.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(com.example.rest.resource.UserResource.class);
+        return classes;
+    }
+}
